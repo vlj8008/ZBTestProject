8ab1946d39e87f76d4c0774f1e7ea59c84d82b0e
Vicky Jones <vlj8008@gmail.com>
2022-06-25 10:51:29 -0400

    Display random team button
    1. Created "Display random team" bootstrap button on html page
    2. Worked on the code to select a random team from the array and display it to screen.
     * This code needs further work. Error message: Uncaught TypeError (line 59 js file)

--- ZBTestProject.js
+++ ZBTestProject.js
@@ -1,45 +1,24 @@
-//perhaps onpage load make a http request to the api
-//and get data.
 
-// Create variable with api URL
+// Create global variable with api URL
 
 var API_URL = " https://www.balldontlie.io/api/v1/teams";
 
-//$.getJSON(API_URL, function(data){
-    
-
-    
-//});
-
-//console.log(data);
-
 // Make an asynchronise request to the api
-
 
 
 async function getTeams(){
     const response = await fetch(API_URL); //use js fetch method and pass in the url as an argument.
     const promiseObj = await response.json(); // json method returns promise object (in our case an array).
-    //var team = JSON.parse(json);
 
-
-
-
-
-    //for (i=0;i<promiseObj.data.length; i=i+1){
-        //console.log(promiseObj.data[i].abbreviation);
-        //let text = document.getElementById("test").textContent;
-        //document.getElementById("test").innerHTML=promiseObj.data[i].abbreviation;
-        console.log(promiseObj);
 
         for (var key in promiseObj) {
             for (var i = 0; i < promiseObj[key].length; i++) {
-                var teamName = promiseObj[key][i].name;
+                var teamName = promiseObj[key][i].name; //extracting properties I want to display
                 var city = promiseObj[key][i].city;
                 var division = promiseObj[key][i].division;
 
                 
-
+                // Creating bootstrap cards to inject json data in to. 
                 var badge = document.createElement('div');
                 badge.className = 'badge col-sm-3 card text-white bg-info mb-2 mt-2 ml-2 mr-2 mb-2 pt-2 pl-2 pr-2style="max-width: 18rem;" ';
                 badge.innerHTML =
@@ -48,23 +27,66 @@ async function getTeams(){
                     '<h3 class="card-text">' + city + '</h3>' +
                     '<h3 class="card-text">' + division + '</h3>'
                     
-
-                //I gave the div the same ID's as the keys in the object for ease
                 document.getElementById(key).appendChild(badge);
             }
         }
     }
 
-    function getRandomTeam(){
+    
+    async function getRandomTeam(){
 
+        // This method must:
+        //1. get the array of objects from the api
+        //2. create a random index (using the array length property)
+        //3. use the random index to access that object in the array
+        //4. display that random object on the html page.
 
-
+        const response = await fetch(API_URL);
+        const responseData = await response.json();
         
-    }
+        //1. get the array of objects from the api
+
+            for(var object in responseData){
+                for (var team = 0; team < responseData[object].length; team++){
+                    
+                    var orderedTeamList = responseData[object][team];
+                    console.log(orderedTeamList); // outputs array of ordered teams. 
+                    
+                }
+
+        //2. create a random index (using the array length property)
+
+                var randomIndex = orderedTeamList.data[Math.floor(Math.random()*orderedTeamList.data.length)];
+                console.log(randomIndex);
+
+        //3. use the random index to access that object in the array
+        //4. display that random object on the html page.
+
+                var randomTeam = document.createElement('div');
+                
+                randomTeam.className = 'badge col-sm-3 card text-white bg-info mb-2 mt-2 ml-2 mr-2 mb-2 pt-2 pl-2 pr-2style="max-width: 18rem;" ';
+                randomTeam.innerHTML =
+                    
+                    '<h1 class="card-header">' +  + '</h1>' +
+                    '<h3 class="card-text">' +  + '</h3>' +
+                    '<h3 class="card-text">' +  + '</h3>'
+                    
+
+                
+                document.getElementById(key).appendChild(randomTeam);
+
+                }
+
+            }
+        
 
     getTeams();
 
     getRandomTeam();
+
+
+
+    
 
     
     
